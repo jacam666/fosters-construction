2a314cd931e5d26991a3b8068c725ab2de922eba
jacam666 <ja6cam@gmail.com>
2024-06-22 12:09:08 +0100

    restyled footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,16 +12,16 @@ export default function Footer() {
                 </div>
 
                 <div className=''>
-                    <Link href='' className=' me-4 text-reset'>
+                    <Link href='' className=' me-4 text-reset text-2xl'>
                         <MDBIcon fab icon="facebook-f" className='text-[#4267B2]' />
                     </Link>
-                    <Link href='' className='me-4 text-reset'>
+                    <Link href='' className='me-4 text-reset text-2xl'>
                         <MDBIcon fab icon="twitter" className="text-[#1DA1F2]" />
                     </Link>
-                    <Link href='' className='me-4 text-reset'>
+                    <Link href='' className='me-4 text-reset text-2xl'>
                         <MDBIcon fab icon="google" className="text-[#DB4437]" />
                     </Link>
-                    <Link href='' className='me-4 text-reset'>
+                    <Link href='' className='me-4 text-reset text-2xl'>
                         <MDBIcon fab icon="instagram" className="text-[#E1306C]" />
                     </Link>
                 </div>
@@ -30,16 +30,16 @@ export default function Footer() {
                 <MDBContainer className='text-center text-md-start mt-5'>
                     <MDBRow className='mt-3'>
                         <MDBCol md="5" lg="4" xl="3" className='mx-auto mb-md-0 mb-4'>
-                            <h6 className='text-uppercase text-center fw-bold mb-4'>Contact</h6>
+                            <h4 className='text-uppercase text-center fw-bold mb-4'>Contact</h4>
                             {/* <p>
                                 <MDBIcon icon="envelope" className="me-3" />
                                 enquiries@mycapeverde.org
                             </p> */}
                             <p>
-                                <MDBIcon icon="phone" className="me-3" /> 07444 365428
+                                <MDBIcon icon="phone" className="me-3 text-2xl" /> 07444 365428
                             </p>
                             <p>
-                            <MDBIcon icon="home" className="me-3" /> Milton Keynes
+                            <MDBIcon icon="home" className="me-3 text-2xl" /> Milton Keynes
                             </p>
                         </MDBCol>
                     </MDBRow>
