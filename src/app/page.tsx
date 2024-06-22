7b4a05b69a6a8bb39a009690ff980507c6d4ad9e
jacam666 <ja6cam@gmail.com>
2024-06-22 11:26:17 +0100

    added new sections to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           <div className=" text-center my-9">
             <Link href='/' target='blank' rel='noopener noreferrer'>
               <button
-                className='flex justify-center items-center  my-8 mx-auto px-4 py-2 rounded-lg  bg-gray-600  text-white font-bold text-xl'
+                className='flex justify-center items-center  my-8 mx-auto px-4 py-2 rounded-lg  bg-gray-700  text-white font-bold text-xl'
               >
                 OUR PROJECTS <IoIosArrowRoundForward className="text-4xl font-bold text-white" />
               </button>
@@ -43,10 +43,10 @@ export default function Home() {
                 width={1000}
                 height={500}
               />
-              <h2>
+              <h2 className="font-anton font-bold mt-2">
                 HIGH QUALITY MATERIALS
               </h2>
-              <p>
+              <p className="mb-5 text-lg">
                 We use only the best materials to ensure durability and satisfaction.
               </p>
             </div>
@@ -57,10 +57,10 @@ export default function Home() {
                 width={1000}
                 height={500}
               />
-              <h2>
+              <h2 className="font-anton font-bold mt-2">
                 EXPERIENCED TEAM
               </h2>
-              <p>
+              <p className="mb-5 text-lg">
                 Our team has years of experience in the construction industry.
               </p>
             </div>
@@ -71,10 +71,10 @@ export default function Home() {
                 width={1000}
                 height={500}
               />
-              <h2>
+              <h2 className="font-anton font-bold mt-2">
                 TIMELY COMPLETION
               </h2>
-              <p>
+              <p className="mb-5 text-lg">
                 We value your time and promise to complete projects within the agreed timeline.
               </p>
             </div>
@@ -82,7 +82,34 @@ export default function Home() {
           </div>
 
         </div>
-
+        <div className="bg-gray-700 text-gray-200 rounded">
+          <p className="p-5 text-2xl">Foster & Sons are dedicated to delivering top-notch construction services for both residential and commercial clients. With over 10 years of industry experience, we guarantee professional, efficient, and reliable service tailored to your specific needs.</p>
+        </div>
+        <div className="bg-gray-700 text-gray-200 p-4 rounded">
+          <h1 className="text-4xl text-center text-white my-3">
+            READY TO BUILD?
+          </h1>
+          <p className="text-xl text-center py-4">
+            Contact us today to start your construction project and turn your vision into reality.
+          </p>
+          <div className=" text-center my-9">
+            <Link href='/' target='blank' rel='noopener noreferrer'>
+              <button
+                className='flex justify-center items-center mb-1 mx-auto px-4 py-2 rounded-lg w-5/6  bg-gray-300  text-gray-700 font-bold text-xl'
+              >
+                GET IN TOUCH
+              </button>
+            </Link>
+            <Link href='/' target='blank' rel='noopener noreferrer'>
+              <button
+                className='flex justify-center items-center mt-1 mx-auto px-4 py-2 rounded-lg w-5/6  bg-gray-300  text-gray-700 font-bold text-xl'
+              >
+                OUR PROJECTS <IoIosArrowRoundForward className="text-4xl font-bold text-gray-700" />
+              </button>
+            </Link>
+          </div>
+        </div>
+        
 
       </div>
     </div>
