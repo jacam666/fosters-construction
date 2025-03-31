30cba5306740f0cb06cff5413856c3e9f20e3f9d
jacam666 <ja6cam@gmail.com>
2025-03-31 21:13:18 +0100

    restyling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,16 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 export default function Home() {
   return (
     <div>
-      <div className="background">
-          <MyNavbar />
+      <div className="background  opacity-95">
+        <MyNavbar />
         <div className="flex justify-center">
-          <h1 className="frontPage-header text-white text-5xl mt-9 py-9 w-3/4 md:text-7xl xl:text-6xl text-center ">Foster &#38; Sons Construction</h1>
+          <h1 className=" text-white text-5xl mt-9 py-9 w-3/4  xl:text-7xl text-center ">Foster &#38; Sons Construction</h1>
         </div>
         <div className="text-center my-9">
-          <p className=" text-3xl lg:text-8xl text-white font-bold">
+          <h1 className="font-sans text-3xl lg:text-8xl text-white font-bold">
             BUILDING YOUR DREAMS
-          </p>
-          <p className="text-2xl text-gray-300 my-9  py-9 mx-auto xl:w-2/3">
+          </h1>
+          <p className="font-sans text-2xl lg:text-3xl text-white my-9 mx-4 py-9 xl:w-2/3">
             Reliable and efficient construction services for residential and commercial properties.
           </p>
           <div className=" text-center my-9 ">
@@ -24,58 +24,58 @@ export default function Home() {
               <button
                 className='flex justify-center items-center my-8 mx-auto px-4 py-2 rounded-lg  bg-gradient from-gray-700 to gray-900  text-gray-100 font-bold text-xl hover:bg-white hover:text-black'
               >
-                OUR PROJECTS <IoIosArrowRoundForward className="text-4xl font-bold text-gray-100 hover:bg-white hover:text-black" />
+                OUR PROJECTS <IoIosArrowRoundForward className="text-4xl font-bold text-white hover:bg-white hover:text-black" />
               </button>
             </Link>
           </div>
         </div>
       </div>
       <div className="bg-white h-full my-9 mx-3 ">
+      <h3 className="font-bold sm:text-3xl ">Why Choose Us?</h3>
+      <div className=" h-1 w-1/2 md:w-1/4 bg-black mt-4 mb-8"></div>
         <div className="sm:flex flex-row ">
           <div className="sm:flex sm:flex-col">
-            <h3 className="font-bold sm:text-lg sm:mr-9">WHY CHOOSE US?</h3>
-            <div className=" h-2 sm:h-1 w-1/2 md:w-4/5 bg-black mt-4 mb-8"></div>
           </div>
-          <div className="sm:flex sm:flex-row">
-            <div className="mx-2">
+          <div className="flex flex-col sm:flex-row">
+            <div className="mx-2 text-center">
               <Image
                 src="/images/homepageimage.jpg"
                 alt="Why Choose Us"
                 width={1000}
                 height={500}
               />
-              <h2 className="font-anton font-bold mt-2 sm:text-xl">
-                HIGH QUALITY MATERIALS
+              <h2 className="font-anton font-bold mt-2 sm:text-3xl">
+                High Quality Materials
               </h2>
-              <p className="mb-5 text-lg sm:text-sm">
+              <p className="mb-5 text-lg sm:text-xl text-gray-900">
                 We use only the best materials to ensure durability and satisfaction.
               </p>
             </div>
-            <div className="mx-2">
+            <div className="mx-2 text-center">
               <Image
                 src="/images/homepageimage-2.jpg"
                 alt="Why Choose Us"
                 width={1000}
                 height={500}
               />
-              <h2 className="font-anton font-bold mt-2 sm:text-xl">
-                EXPERIENCED TEAM
+              <h2 className="font-anton font-bold mt-2 sm:text-3xl">
+                Experienced Team
               </h2>
-              <p className="mb-5 text-lg sm:text-sm">
+              <p className="mb-5 text-lg sm:text-xl text-gray-900">
                 Our team has years of experience in the construction industry.
               </p>
             </div>
-            <div className="mx-2">
+            <div className="mx-2 text-center">
               <Image
                 src="/images/homeimage-3.jpg"
                 alt="Why Choose Us"
                 width={1000}
                 height={500}
               />
-              <h2 className="font-anton font-bold mt-2 sm:text-xl">
-                TIMELY COMPLETION
+              <h2 className="font-anton font-bold mt-2 sm:text-3xl">
+                Timely Completion
               </h2>
-              <p className="mb-5 text-lg sm:text-sm">
+              <p className="mb-5 text-lg sm:text-xl text-gray-900">
                 We value your time and promise to complete projects within the agreed timeline.
               </p>
             </div>
@@ -85,26 +85,27 @@ export default function Home() {
         {/* <div className="bg-gray-700 text-gray-200 rounded">
           <p className="p-5 text-2xl md:text-4xl lg:px-9">Foster & Sons are dedicated to delivering top-notch construction services for both residential and commercial clients. With years of industry experience, we guarantee professional, efficient, and reliable service tailored to your specific needs.</p>
         </div> */}
-        <div className="bg-gray-700 text-gray-200 p-4 rounded">
-          <h1 className="text-4xl text-center text-white my-3">
+        <div className="flex flex-col bg-sky-900 text-gray-200 p-4 rounded">
+          <h1 className="text-4xl text-center font-bold text-white my-3">
             READY TO BUILD?
           </h1>
-          <p className="text-xl text-center py-4">
+          <p className="text-xl text-center md:text-3xl py-4">
             Contact us today to start your construction project and turn your vision into reality.
           </p>
-          <div className=" text-center my-9 md:flex md:flex-row md:justify-center">
-            <Link href='/' target='blank' rel='noopener noreferrer'>
+          <div className=" flex flex-row justify-center  w-full text-center my-6">
+            <Link href='/contactUs' target='blank' rel='noopener noreferrer'>
               <button
-                className='flex justify-center items-center my-1 mx-auto px-4 py-2 rounded-lg w-5/6 md:w-11/12  bg-gray-300  text-gray-700 font-bold text-xl'
+                className='  sm:mx-6 mx-2 px-3 py-2 rounded-lg bg-sky-400  text-gray-900 font-bold text-xl hover:bg-sky-600 hover:text-white'
               >
-                GET IN TOUCH
+                Contact Us
               </button>
             </Link>
             <Link href='/projectGallery' target='blank' rel='noopener noreferrer'>
               <button
-                className='flex justify-center items-center my-1 mx-auto px-4 py-2 rounded-lg w-5/6 md:w-11/12  bg-gray-300  text-gray-700 font-bold text-xl'
+                className='sm:mx-6 mx-2 px-3 py-2 rounded-lg bg-sky-400 text-gray-900 font-bold text-xl hover:bg-sky-600 hover:text-white '
               >
-                OUR PROJECTS <IoIosArrowRoundForward className="text-4xl font-bold text-gray-700" />
+                Our Projects
+                {/* Our Projects <IoIosArrowRoundForward className="text-4xl font-bold text-gray-700" /> */}
               </button>
             </Link>
           </div>
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -3,47 +3,47 @@
 import React from 'react';
 import MyNavbar from "@/components/MyNavbar";
 import Footer from "@/components/Footer";
-import { FaHammer, FaHome, FaTree, FaRoad } from 'react-icons/fa'; // Importing icons from react-icons
+import { FaHammer, FaHome, FaTree, FaRoad } from 'react-icons/fa';
 import { NextPage } from 'next';
 import { FaDoorOpen } from 'react-icons/fa6';
 import Image from 'next/image';
 
 const services = [
     {
-        icon: <FaHammer />,
+        icon: <FaHammer className='text-sky-900' />,
         title: "Carpentry",
         description: "Expert carpentry services, providing high-quality woodwork for your home and office.",
         image: "/images/galleryImages/galleryimage5.jpg"
     },
     {
-        icon: <FaHome />,
+        icon: <FaHome className='text-sky-900' />,
         title: "Building Extensions",
         description: "Extend your home with our professional building extension services, customized to your needs.",
         image: "/images/galleryImages/galleryimage16.jpg"
 
     },
     {
-        icon: <FaHome />, // Add another icon here if needed
+        icon: <FaHome className='text-sky-900' />, 
         title: "Roofing",
         description: "Our expert team offers secure roofing services to protect your home from all weather conditions.",
         image: "/images/galleryImages/galleryimage17.jpg"
 
     },
     {
-        icon: <FaTree />,
+        icon: <FaTree className='text-sky-900' />,
         title: "Gardens",
         description: "Transform your outdoor space with our garden design and landscaping services.",
         image: "/images/galleryImages/galleryimage18.jpg"
 
     },
     {
-        icon: <FaRoad />,
+        icon: <FaRoad className='text-sky-900' />,
         title: "Driveways",
         description: "Transform the aesthetic appeal of your residence by enhancing your home's driveway",
         image: "/images/galleryImages/galleryimage10.jpg"
     },
     {
-        icon: <FaDoorOpen />,
+        icon: <FaDoorOpen className='text-sky-900' />,
         title: "Windows and Doors",
         description: "High-quality windows and doors designed to improve the aesthetics and security of your home.",
         image: "/images/galleryImages/galleryimage1.jpg"
@@ -54,23 +54,22 @@ const ServicesPage: NextPage = () => {
     return (
         <div>
             <div className='gallery-background'>
-            <MyNavbar />
+                <MyNavbar />
             </div>
-            <div className="container mx-auto px-4 py-8">
-                <h1 className="frontPage-header text-center text-6xl text-gray-800 font-bold py-8">Our Services</h1>
+            <div className="container mx-auto  py-8">
+                <h1 className=" text-center text-6xl text-gray-900 font-bold py-8">Our Services</h1>
                 <div className="services-grid my-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {services.map((service, index) => (
                         <div key={index} className="service-card mb-4 shadow-md p-6 text-center rounded-md flex flex-col justify-between">
-                            
                             <div className="service-icon text-5xl mb-4">{service.icon}</div>
-                            <h2 className="font-bold text-2xl">{service.title}</h2>
-                            <p className="text-gray-600 mt-4">{service.description}</p>
-                            <Image 
-                            src={service.image || "/images/defaultImage.jpg"}
-                            alt={service.title}
-                            className='service-image mx-auto rounded-lg mt-4'
-                            width={200}
-                            height={200}
+                            <h2 className="font-bold text-4xl text-gray-900 md:text-5xl">{service.title}</h2>
+                            <p className="text-gray-900 md:text-2xl mt-4">{service.description}</p>
+                            <Image
+                                src={service.image || "/images/defaultImage.jpg"}
+                                alt={service.title}
+                                className='w-full service-image rounded-lg mt-4'
+                                width={200}
+                                height={200}
                             />
                         </div>
                     ))}
